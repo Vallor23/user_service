1765aa955102f1dfb92b990f16ade54c4295800c
Vallor23 <enosnyabinda2017@gmail.com>
2025-07-22 16:54:45 +0300

    create usermodel for user registration and login

---
+++ users/views.py
@@ -0,0 +1,13 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import User
+from .serializers import UserSerializer
+
+# Create your views here.
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
